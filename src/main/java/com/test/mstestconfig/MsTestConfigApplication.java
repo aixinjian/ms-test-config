89ececcb5fee29850068d308b6392719e0cf3ef8
aixinjian <xinjian.ai@hosjoy.com>
2019-11-13 15:05:09 +0800

    修改注解

--- src/main/java/com/test/mstestconfig/MsTestConfigApplication.java
+++ src/main/java/com/test/mstestconfig/MsTestConfigApplication.java
@@ -2,10 +2,11 @@ package com.test.mstestconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaClient
 public class MsTestConfigApplication {
 
 	public static void main(String[] args) {
